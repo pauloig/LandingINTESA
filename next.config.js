a811633cd31ded5cea4e2c842efabd6f87909f72
Paulo Ismalej <paulo@Mac-mini-de-Paulo.local>
2025-05-05 23:16:14 -0600

    Update next.config.js for image handling

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true,
   },
 }
 
